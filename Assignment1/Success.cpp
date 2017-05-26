af9b9cb26027d4cc6aed16fb2c041eb64f7e2e31
Mayank <mayank@Mayanks-MacBook-Pro.local>
2017-05-26 23:34:39 +0530

    Update //				cout<<j<<endl;

--- Assignment1/Success.cpp
+++ Assignment1/Success.cpp
@@ -5,7 +5,7 @@ int main(){
 	cin>>t;
 	int a;
 	for(int it=0;it<t;it++){
-		int x,y,p,q;
+		long long int x,y,p,q;
 		cin>>x>>y>>p>>q;
 		if(p==q){
 			if(x==y) cout<<"0"<<endl;
@@ -16,18 +16,36 @@ int main(){
 			else cout<<"-1"<<endl;
 		}
 		else{
-			int k=x%p;
-			
-			int j=(p*y-q*x)/(q-p);
-			float o=(float)(p*y-q*x)/(q-p);
-			if(o>0){
-			int temp=j%p;
-			j=j+p-temp;
+			if(p*y-q*x>0){
+				long long int k;
+				long long int num=p*y-q*x;
+				long long int den=q-p;
+				long long int nfrac=num/den;
+				double frac=(float)num/den;
+				long long int alfa;
+				if(frac-nfrac) alfa=nfrac+1;
+				else alfa=nfrac;
+//				cout<<alfa<<endl;
+				long long int temp=x%p;
+				long long int temp2=alfa%p;
+				if(temp==0){if(temp2!=0)  alfa=alfa+p-temp2;}
+				else{
+					if(p-temp<temp2) alfa=alfa+p-temp-temp2+p;
+					else alfa=alfa+p-temp-temp2;
+				}
+			//	cout<<alfa<<endl;
+				cout<<(alfa+x)/p*q-y;
 			}
-			
-
-			cout<<
-			
+			else if(p*y<q*x){
+				long long int k=x%p;
+				long long int j=0;
+				if(k!=0)  j=p-k;
+//				cout<<j<<endl;
+				cout<<(x+j)/p*q-y;
+			}
+			else cout<<"0";
+		cout<<endl;
 		}
+		
 	}
 }
